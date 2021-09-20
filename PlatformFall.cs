e59cc2d0ba64ea2b72bce567069f6f65d99b6d20
MageStep <77864237+MageStep@users.noreply.github.com>
2021-09-20 10:53:21 -0500

    Add files via upload

--- PlatformFall.cs
+++ PlatformFall.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlatformFall : MonoBehaviour
 {
 
+    public AnimationCurve curve;
+
     Rigidbody rb;
 
     Vector3 startingPosition;
@@ -18,13 +20,36 @@ public class PlatformFall : MonoBehaviour
 
     void OnTriggerEnter(Collider other) {
         if(other.gameObject.CompareTag("Player")) {
-            rb.isKinematic = false;
+            StartCoroutine(ResetPlatform());
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ResetPlatform()
     {
-        
+        yield return new WaitForSeconds(1);             // platform has a delay before falling
+        rb.isKinematic = false;                         // let the platform fall
+        yield return new WaitForSeconds(3);             // wait 3 seconds
+        this.transform.position = startingPosition;  
+          //reset the platform position   
+        rb.isKinematic = true; 
+        StartCoroutine(LerpPosition());                         // make the platform not move
     }
+
+    IEnumerator LerpPosition() {
+        Vector3 endPosition = this.transform.position;
+
+        float elapsedTime = 0f;
+
+        float returnInterval = 1f;
+
+        while(elapsedTime < returnInterval) 
+        {
+            this.transform.position = Vector3.Lerp(endPosition, startingPosition, curve.Evaluate(elapsedTime / returnInterval));
+            yield return null;
+        }
+
+    }
+
 }
+
+    
