191a4641300326b6dba54550814bf6864d1f0bf3
MageStep <77864237+MageStep@users.noreply.github.com>
2021-10-18 10:51:39 -0500

    Add files via upload

--- UIController.cs
+++ UIController.cs
@@ -1,18 +1,67 @@
+// this should be on the canvas
+// this controls all of the Screenspace UI in the level
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
-
 public class UIController : MonoBehaviour
 {
-    public TextMeshProUGUI scoreText;
-    
-    private int totalCoins = 0;
-    
-    public void UpdateScore(int givenChange = 1) 
+
+    [SerializeField]
+    //instead of making this public, we can add it to the inspector with [SerializeField]
+    private Slider healthSlider, manaSlider, xpSlider;            // we can make multiple variables on the same line!
+
+    // getters and setters
+
+    void Start()
     {
-        totalCoins += givenChange;
-        scoreText.text = "Coins: " + totalCoins;
+
+    }
+
+    //setter function
+    public void SetHealthSlider(int amount)
+    {
+        //make sure no one is sending bad data...
+        if(amount < 0)
+        {       
+            healthSlider.value = 0;
+        }
+        if(amount > 100)
+        {
+            healthSlider.value = 100;
+        }
+
+        healthSlider.value = amount;
+    }
+
+    public void SetManaSlider(int amount)
+    {
+        //make sure no one is sending bad data...
+        if(amount < 0)
+        {       
+            manaSlider.value = 0;
+        }
+        if(amount > 100)
+        {
+            manaSlider.value = 100;
+        }
+
+        manaSlider.value = amount;
+    }
+
+        public void SetXPSlider(int amount)
+    {
+        //make sure no one is sending bad data...
+        if(amount < 0)
+        {       
+            xpSlider.value = 0;
+        }
+        if(amount > 100)
+        {
+            xpSlider.value = 100;
+        }
+
+        xpSlider.value = amount;
     }
 }
